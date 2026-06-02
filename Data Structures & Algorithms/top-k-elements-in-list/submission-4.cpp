7d516f1029132ad4936e6df922144d66afe6632e
Ten <uncle10indahouse@gmail.com>
2026-06-02 18:02:47 +0700

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // freq by a[i]
+        unordered_map<int,int> myMap; // key: a[i] | data: count 
+        for (const int& x: nums){
+            myMap[x]++;
+        }
+        // freq by count
+        vector<vector<int>> freq(nums.size()+1);
+        for (const auto& [key,data] : myMap){
+            freq[data].emplace_back(key);
+        }
+        
+        vector<int> res;
+        for (int i = freq.size()-1; i > 0; i--){
+            for (const int& x: freq[i]){
+                res.push_back(x);
+                if (res.size() == k) return res;
+            }
+        }
+        return res;
+    }
+};
