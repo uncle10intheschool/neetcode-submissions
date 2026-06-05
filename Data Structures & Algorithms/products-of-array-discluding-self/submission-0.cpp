c8eb1196b6c59afbc30c482e1e8c77956f9c30fd
Ten <uncle10indahouse@gmail.com>
2026-06-05 11:28:34 +0700

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix(n,1);
+        vector<int> suffix(n,1);
+        for (int i = 1; i < n; i++){
+            prefix[i] = prefix[i-1]*nums[i-1]; // trái X = tích trước nữa X * tích trước X
+        }
+
+        for (int i = n-2; i >= 0; i--){
+            suffix[i] = suffix[i+1]*nums[i+1];
+        }
+
+        for (int i = 0; i < n; i++){
+            prefix[i]*=suffix[i];
+        }
+        return prefix;
+    }
+};
